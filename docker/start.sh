3747cbbd58a50acfec1f17d4f3b5eb7358ad04a0
eeehit <eeehit.sytem@gmail.com>
2016-09-22 22:33:56 +0900

    directory modification

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,2 @@
+sudo docker rm -f home
 sudo docker run --name home -p 8000:80 -v /opt/Home/webroot/app/:/var/www/laravel/app/ -v /opt/Home/webroot/routes/:/var/www/laravel/routes/ -v /opt/Home/webroot/resources:/var/www/laravel/resources -d eboraas/laravel
