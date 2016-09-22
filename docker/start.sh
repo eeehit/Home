17166a7be4ab408ff9ef167b9ea8ebb5a4798dfd
eeehit <eeehit.sytem@gmail.com>
2016-09-22 13:51:08 +0900

    modification error df laravel and docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,1 @@
-sudo docker run --name home -p 8000:80 -v /opt/Home/webroot/app/:/var/www/laravel/app/ -v /opt/Home/webroot/public/:/var/www/laravel/public/ -d eboraas/laravel
+sudo docker run --name home -p 8000:80 -v /opt/Home/webroot/app/:/var/www/laravel/app/ -v /opt/Home/webroot/routes/:/var/www/laravel/routes/ -v /opt/Home/webroot/resources:/var/www/laravel/resources -d eboraas/laravel
